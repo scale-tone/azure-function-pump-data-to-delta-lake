6d037a15fa58e6bb96a4923e7e8785ff9e7691b7
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2022-12-23 22:12:17 +0100

    fix35

--- service-bus-to-delta-lake-func/startup.sh
+++ service-bus-to-delta-lake-func/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ -z "${INPUT_QUEUE_NAME}" ];
+if [ -n $INPUT_QUEUE_NAME ];
 then
-	cp -rf /home/site/wwwroot/ServiceBusTrigger/function-for-topic.json /home/site/wwwroot/ServiceBusTrigger/function.json
-else
 	cp -rf /home/site/wwwroot/ServiceBusTrigger/function-for-queue.json /home/site/wwwroot/ServiceBusTrigger/function.json
+else
+	cp -rf /home/site/wwwroot/ServiceBusTrigger/function-for-topic.json /home/site/wwwroot/ServiceBusTrigger/function.json
 fi
 
 /azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost
