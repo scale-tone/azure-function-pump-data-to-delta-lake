f03c85f351a81497004e91f4a235c3f953d6542c
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2023-06-05 10:58:36 +0200

    renaming

--- pump-to-delta-lake-func/startup.sh
+++ pump-to-delta-lake-func/startup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -n "${STORAGE_CONN_STRING}" ];
+then
+	cp -rf /home/site/wwwroot/QueueTrigger/function-for-storage-queue.json /home/site/wwwroot/QueueTrigger/function.json
+fi
+
 if [ -n "${EVENTHUB_CONN_STRING}" ];
 then
 	cp -rf /home/site/wwwroot/EventHubTrigger/function-for-eventhub.json /home/site/wwwroot/EventHubTrigger/function.json
@@ -8,10 +13,11 @@ fi
 if [ -n "${SERVICEBUS_CONN_STRING}" ];
 then
 
-	if [ -n "${INPUT_QUEUE_NAME}" ];
+	if [ -n "${SERVICEBUS_QUEUE_NAME}" ];
 	then
 		cp -rf /home/site/wwwroot/ServiceBusTrigger/function-for-queue.json /home/site/wwwroot/ServiceBusTrigger/function.json
-	else
+	elif [ -n "${SERVICEBUS_TOPIC_NAME}" ];
+	then
 		cp -rf /home/site/wwwroot/ServiceBusTrigger/function-for-topic.json /home/site/wwwroot/ServiceBusTrigger/function.json
 	fi
 
