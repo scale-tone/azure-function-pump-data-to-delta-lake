8c05ea8bb86446da7eedd29a9204079f8316d350
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2023-06-08 17:14:47 +0200

    batching for storage queues

--- pump-to-delta-lake-func/QueueTrigger/__init__.py
+++ pump-to-delta-lake-func/QueueTrigger/__init__.py
@@ -1,8 +1,26 @@
+import time
 from typing import List
 import azure.functions as func
 from helpers import convert_and_add_message, send_to_delta_table
 
-def main(events: List[func.QueueMessage]) -> None:
+batch_size = 100
+# waiting max 3 seconds
+max_wait_count = 60
+events = []
+
+def main(event: func.QueueMessage) -> None:
+
+    events.append(event)
+
+    i = 0
+    while (len(events) < batch_size) and (i < max_wait_count):
+
+        if len(events) == 0:
+            # if the batch got processed by another instance, just returning
+            return
+
+        i = i + 1
+        time.sleep(0.05)
 
     result = []
 
@@ -10,3 +28,5 @@ def main(events: List[func.QueueMessage]) -> None:
         convert_and_add_message(event.get_body().decode('utf-8'), result)
 
     send_to_delta_table(result)
+
+    events.clear()
