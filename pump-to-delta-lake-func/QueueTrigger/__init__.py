e6d8bf945614175175e2bfd52c71c0521a3f0fe8
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2023-06-12 03:11:58 +0200

    another try

--- pump-to-delta-lake-func/QueueTrigger/__init__.py
+++ pump-to-delta-lake-func/QueueTrigger/__init__.py
@@ -5,14 +5,88 @@ import tempfile
 import shutil
 import time
 import math
+import json
+import xmltodict
 
 import azure.functions as func
-from helpers import convert_and_add_message, send_to_delta_table
+
+
+from delta.tables import *
+from pyspark.sql import SparkSession
+from jsonpath_ng import parse
+
+# Saving DataBricks connection info into its config file
+databricksConnectSettings = { \
+    "host": os.environ["DATABRICKS_ADDRESS"], \
+    "token": os.environ["DATABRICKS_API_TOKEN"], \
+    "cluster_id": os.environ["DATABRICKS_CLUSTER_ID"], \
+    "org_id": os.environ["DATABRICKS_ORG_ID"], \
+    "port": "15001" \
+}
+
+with open('/root/.databricks-connect', 'w') as configFile:
+    json.dump(databricksConnectSettings, configFile, ensure_ascii=False)
+
+# Creating spark session
+spark = SparkSession.builder.getOrCreate()
+
+def send_to_delta_table_local(msgs):
+
+    dFrame = spark.createDataFrame(msgs)
+
+    # Appending records to OUTPUT_TABLE_NAME
+    dFrame.write.format("delta").mode("append").saveAsTable(os.environ["OUTPUT_TABLE_NAME"])
+
+
+def apply_jsonpath(msg, json_path):
+    
+    results = parse(json_path).find(msg)
+
+    if len(results) == 1:
+        # jsonpath produced an object - just returning it
+        return results[0].value
+    else:
+        # jsonpath produced an array - trying to convert it back into object
+        record = {}
+        for res in results:
+            record[res.path.fields[0]] = res.value
+
+        return record
+
+
+# Will apply this jsonpath query, if specified
+jsonPathQuery = os.getenv("JSONPATH_QUERY")
+
+def convert_and_add_message(msgString, convertedMsgBatch):
+
+    # supporting both XML and JSON
+    if msgString.startswith("<"):
+        jsonObjects = xmltodict.parse(msgString, attr_prefix="")
+        jsonObjects = list(jsonObjects.values())[0]
+    else:
+        jsonObjects = json.loads(msgString)
+
+    # converting to array, if it is not yet
+    if type(jsonObjects) != list:
+        jsonObjects = [jsonObjects]
+
+    for jsonObject in jsonObjects:
+
+        # Applying jsonpath query, if specified
+        if jsonPathQuery != None:
+            jsonObject = apply_jsonpath(jsonObject, jsonPathQuery)
+
+        convertedMsgBatch.append(jsonObject)
+
+
+
 
 buf_dir = os.path.join(tempfile.gettempdir(), "pump-to-delta-lake-func-buf")
 os.makedirs(buf_dir, exist_ok=True)
 
 MAX_WAIT_IN_SECONDS = 300
+
+
 
 # Storage Queue trigger does not support batching. So we'll need to handcraft it ourselves, using local temp folder as a buffer.
 # The strategy below is as follows:
@@ -58,7 +132,7 @@ def main(event: func.QueueMessage) -> None:
                 logging.warning(f">> sending {len(result)} events ({event.id})")
 
                 # Sending batch to Delta Table
-                send_to_delta_table(result)
+                send_to_delta_table_local(result)
 
                 logging.warning(f">> successfully sent {len(result)} events ({event.id})")
 
