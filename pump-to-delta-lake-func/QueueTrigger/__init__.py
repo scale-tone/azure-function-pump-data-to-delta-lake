692bcaecca29819b02ff6ba277d71249c4872f79
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2023-06-12 12:41:27 +0200

    queue fix

--- pump-to-delta-lake-func/QueueTrigger/__init__.py
+++ pump-to-delta-lake-func/QueueTrigger/__init__.py
@@ -5,92 +5,20 @@ import tempfile
 import shutil
 import time
 import math
-import json
-import xmltodict
 
 import azure.functions as func
-
-
-from delta.tables import *
-from pyspark.sql import SparkSession
-from jsonpath_ng import parse
-
-# Saving DataBricks connection info into its config file
-databricksConnectSettings = { \
-    "host": os.environ["DATABRICKS_ADDRESS"], \
-    "token": os.environ["DATABRICKS_API_TOKEN"], \
-    "cluster_id": os.environ["DATABRICKS_CLUSTER_ID"], \
-    "org_id": os.environ["DATABRICKS_ORG_ID"], \
-    "port": "15001" \
-}
-
-with open('/root/.databricks-connect', 'w') as configFile:
-    json.dump(databricksConnectSettings, configFile, ensure_ascii=False)
-
-# Creating spark session
-spark = SparkSession.builder.getOrCreate()
-
-def send_to_delta_table_local(msgs):
-
-    dFrame = spark.createDataFrame(msgs)
-
-    # Appending records to OUTPUT_TABLE_NAME
-    dFrame.write.format("delta").mode("append").saveAsTable(os.environ["OUTPUT_TABLE_NAME"])
-
-
-def apply_jsonpath(msg, json_path):
-    
-    results = parse(json_path).find(msg)
-
-    if len(results) == 1:
-        # jsonpath produced an object - just returning it
-        return results[0].value
-    else:
-        # jsonpath produced an array - trying to convert it back into object
-        record = {}
-        for res in results:
-            record[res.path.fields[0]] = res.value
-
-        return record
-
-
-# Will apply this jsonpath query, if specified
-jsonPathQuery = os.getenv("JSONPATH_QUERY")
-
-def convert_and_add_message(msgString, convertedMsgBatch):
-
-    # supporting both XML and JSON
-    if msgString.startswith("<"):
-        jsonObjects = xmltodict.parse(msgString, attr_prefix="")
-        jsonObjects = list(jsonObjects.values())[0]
-    else:
-        jsonObjects = json.loads(msgString)
-
-    # converting to array, if it is not yet
-    if type(jsonObjects) != list:
-        jsonObjects = [jsonObjects]
-
-    for jsonObject in jsonObjects:
-
-        # Applying jsonpath query, if specified
-        if jsonPathQuery != None:
-            jsonObject = apply_jsonpath(jsonObject, jsonPathQuery)
-
-        convertedMsgBatch.append(jsonObject)
-
-
-
+from helpers import convert_and_add_message, send_to_delta_table
 
 buf_dir = os.path.join(tempfile.gettempdir(), "pump-to-delta-lake-func-buf")
 os.makedirs(buf_dir, exist_ok=True)
 
+PUSH_INTERVAL_IN_SECONDS = 10
 MAX_WAIT_IN_SECONDS = 300
-
 
 
 # Storage Queue trigger does not support batching. So we'll need to handcraft it ourselves, using local temp folder as a buffer.
 # The strategy below is as follows:
-#   We store events in bucket folders, a separate folder _per every second_.
+#   We store events in bucket folders, a separate folder _per every PUSH_INTERVAL_IN_SECONDS_.
 #   Then one of the handlers obtains an exclusive lock over that folder (by exclusively creating a dummy file)
 #   and pushes all files from it to destination. Other handlers just wait until it happens.
 #   If it doesn't happen within MAX_WAIT_IN_SECONDS, the handlers throw, causing their messages to be retried.
@@ -98,7 +26,7 @@ def main(event: func.QueueMessage) -> None:
 
     # Organizing a 1-second-long bucket folder
     now = datetime.datetime.now()
-    str_time = now.strftime("%Y-%m-%d-%H-%M-") + str(math.floor(now.second / 10))
+    str_time = now.strftime("%Y-%m-%d-%H-%M-") + str(math.floor(now.second / PUSH_INTERVAL_IN_SECONDS))
 
     batch_dir = os.path.join(buf_dir, str_time)
     os.makedirs(batch_dir, exist_ok=True)
@@ -109,8 +37,8 @@ def main(event: func.QueueMessage) -> None:
         with open(cur_event_file_path, 'w') as f:
             f.write(event.get_body().decode('utf-8'))
 
-        # Sleeping for 1 second, to ensure all events belonging to current bucket have arrived
-        time.sleep(1)
+        # Sleeping for PUSH_INTERVAL_IN_SECONDS second, to ensure all events belonging to current bucket have arrived
+        time.sleep(PUSH_INTERVAL_IN_SECONDS)
 
         # Now one handler should do the job of sending events, others should just wait until it happens.
         # If that doesn't happen, then all handlers should throw (so that all events are retried)
@@ -132,7 +60,7 @@ def main(event: func.QueueMessage) -> None:
                 logging.warning(f">> sending {len(result)} events ({event.id})")
 
                 # Sending batch to Delta Table
-                send_to_delta_table_local(result)
+                send_to_delta_table(result)
 
                 logging.warning(f">> successfully sent {len(result)} events ({event.id})")
 
